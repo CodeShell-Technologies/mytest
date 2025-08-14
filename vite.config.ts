175e9614d6f2af4e468c89acfbeead742c5e8ea1
CodeShell-Technologies <scodeshell@gmail.com>
2025-08-14 10:25:08 +0530

    update on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  
- 
+  ssr: {
+    noExternal: ["react-datepicker", "clsx"], // 👈 This forces Vite to transpile them
+  },
 });
